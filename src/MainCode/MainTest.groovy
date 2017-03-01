361932867eece5adcdb1c0442b1df2780868007e
BaoTNM <BaoTNguyenM@gmail.com>
2017-03-01 16:14:47 +0100

    add test class

---
+++ src/MainCode/MainTest.groovy
@@ -0,0 +1,15 @@
+package MainCode
+
+/**
+ * Created by BaoTNM on 01.03.2017.
+ */
+class MainTest extends groovy.util.GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void testMain() {
+
+    }
+}
